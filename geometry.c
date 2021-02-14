cc7018e73742457e68f269a715cb8c726f8db879
Nikolay Kavirshin <kolyancik02@gmail.com>
2021-02-14 20:47:10 +0700

    Create input of object

--- geometry.c
+++ geometry.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-
+    int i, m;
+    int max_symb = 40;
+    char sourse_str[max_symb], coordinates_str[max_symb], radius[max_symb];
+    printf("Введите координаты центра окружности и её радиус.\n");
+    printf("Формат записи: object(x y, r).\n");
+    fgets(sourse_str, max_symb, stdin);
+    fputs(sourse_str, stdout);
 }
