0db745657934b5bd14c9e241cf3f463f68317670
Nikolay Kavirshin <kolyancik02@gmail.com>
2021-05-05 18:12:22 +0700

    write tests for function validation

--- test/test_geometry.c
+++ test/test_geometry.c
@@ -1,11 +1,78 @@
 #include "ctest.h"
 #include "libgeometry/geometry.h"
 
-CTEST(get_radius, correct_input)
+CTEST(validation, correct_input)
 {
-   char input_str[] = "circle(1.00 2.00, 3.00)\n";
-   
-   double expect = 3.00;
-   double result = get_radius(input_str);
-   ASSERT_EQUAL(expect, result);
+    char input_str[] = "circle(123.00 12.00, 5784.00)\n";
+
+    int expect = 0;
+    int result = validation(input_str);
+    ASSERT_EQUAL(expect, result);
 }
+
+CTEST(validation, invalid_object)
+{
+    char input_str[] = "ciocle(123.00 12.00, 5784.00)\n";
+
+    int expect = -2;
+    int result = validation(input_str);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(validation, invalid_x)
+{
+    char input_str[] = "circle(123.\00 12.00, 5784.00)\n";
+
+    int expect = -1;
+    int result = validation(input_str);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(validation, invalid_y)
+{
+    char input_str[] = "circle(123.00 75j9.00, 5784.00)\n";
+
+    int expect = -1;
+    int result = validation(input_str);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(validation, miss_comma)
+{
+    char input_str[] = "circle(123.00 12.00 5784.00)\n";
+
+    int expect = -5;
+    int result = validation(input_str);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(validation, miss_open_bracket)
+{
+    char input_str[] = "circle123.00 12.00, 5784.00)\n";
+
+    int expect = -3;
+    int result = validation(input_str);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(validation, miss_close_bracket)
+{
+    char input_str[] = "circle(123.00 12.00, 5784.00\n";
+
+    int expect = -6;
+    int result = validation(input_str);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(validation, miss_space)
+{
+    char input_str[] = "circle(123.00v12.00, 5784.00)\n";
+
+    int expect = -4;
+    int result = validation(input_str);
+    ASSERT_EQUAL(expect, result);
+}
+
+
+
+
